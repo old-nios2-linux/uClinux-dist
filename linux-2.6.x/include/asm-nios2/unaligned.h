d1eca0d835a5318862b7a074c40715bb9a17907c
Thomas Chou <thomas@wytron.com.tw>
2008-05-07 22:25:59 +0800

    nios2: Move arches to use common unaligned access
    Follow kernel commit 6510d41954dc6a9c8b1dbca7eaca0f23195ca727,
    
    Unaligned access is ok for the following arches:
    cris, m68k, mn10300, powerpc, s390, x86
    
    Arches that use the memmove implementation for native endian, and
    the byteshifting for the opposite endianness.
    h8300, m32r, xtensa
    
    Packed struct for native endian, byteshifting for other endian:
    alpha, blackfin, ia64, parisc, sparc, sparc64, mips, sh
    
    m86knommu is generic_be for Coldfire, otherwise unaligned access is ok.
    
    frv, arm chooses endianness based on compiler settings, uses the byteshifting
    versions.  Remove the unaligned trap handler from frv as it is now unused.
    
    v850 is le, uses the byteshifting versions for both be and le.
    
    Remove the now unused asm-generic implementation.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- linux-2.6.x/include/asm-nios2/unaligned.h
+++ linux-2.6.x/include/asm-nios2/unaligned.h
@@ -1,6 +1,11 @@
 #ifndef __NIOS2_UNALIGNED_H
 #define __NIOS2_UNALIGNED_H
 
-#include <asm-generic/unaligned.h>
+#include <linux/unaligned/be_byteshift.h>
+#include <linux/unaligned/le_byteshift.h>
+#include <linux/unaligned/generic.h>
+
+#define get_unaligned	__get_unaligned_le
+#define put_unaligned	__put_unaligned_le
 
 #endif				/* __NIOS2_UNALIGNED_H */
