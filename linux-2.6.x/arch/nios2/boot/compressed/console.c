a22be4b1fdf6410c7ad138c6a29445ec9c64daed
Thomas Chou <thomas@wytron.com.tw>
2008-06-02 22:29:55 +0800

    nios2: fix uart console base address
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- linux-2.6.x/arch/nios2/boot/compressed/console.c
+++ linux-2.6.x/arch/nios2/boot/compressed/console.c
@@ -1,14 +1,15 @@
 #include <asm/nios2.h>
 #include <asm/io.h>
 
-static void *__ioremap(unsigned long physaddr, unsigned long size, int cacheflag)
+void *__ioremap(unsigned long physaddr, unsigned long size, int cacheflag)
 {
-	return (cacheflag == IOMAP_FULL_CACHING) ? (void *)(physaddr & ~0x80000000) : 
-		(void *)(physaddr | 0x80000000);
+	return (cacheflag == IOMAP_FULL_CACHING) ?
+	    (void *)(physaddr & ~0x80000000) : (void *)(physaddr | 0x80000000);
 }
 
 #if defined(CONFIG_SERIAL_ALTERA_JTAGUART_CONSOLE)
 
+#define ALTERA_JTAGUART_SIZE                      8
 #define ALTERA_JTAGUART_DATA_REG                  0
 #define ALTERA_JTAGUART_CONTROL_REG               4
 #define ALTERA_JTAGUART_CONTROL_AC_MSK            (0x00000400)
@@ -39,13 +40,14 @@ static int putchar(int ch)
 
 static void console_init(void)
 {
-	uartbase = ioremap(na_jtag_uart, 8);
+	uartbase = (unsigned long)ioremap(na_jtag_uart, ALTERA_JTAGUART_SIZE);
 	writel(ALTERA_JTAGUART_CONTROL_AC_MSK,
 	       uartbase + ALTERA_JTAGUART_CONTROL_REG);
 }
 
 #elif defined(CONFIG_SERIAL_ALTERA_UART_CONSOLE)
 
+#define ALTERA_UART_SIZE                  32
 #define ALTERA_UART_TXDATA_REG            4
 #define ALTERA_UART_STATUS_REG            8
 #define ALTERA_UART_DIVISOR_REG           16
@@ -57,11 +59,11 @@ static void uart_putc(int ch)
 	int i;
 
 	for (i = 0; (i < 0x10000); i++) {
-		if (readw(base + ALTERA_UART_STATUS_REG) &
+		if (readw(uartbase + ALTERA_UART_STATUS_REG) &
 		    ALTERA_UART_STATUS_TRDY_MSK)
 			break;
 	}
-	writeb(ch, base + ALTERA_UART_TXDATA_REG);
+	writeb(ch, uartbase + ALTERA_UART_TXDATA_REG);
 }
 
 static int putchar(int ch)
@@ -74,13 +76,13 @@ static int putchar(int ch)
 
 static void console_init(void)
 {
-	unsigned base = (unsigned)na_uart0;
 	unsigned int baud, baudclk;
 
-	uart_base = ioremap(na_uart0, 32);
+	uartbase =
+	    (unsigned long)ioremap((unsigned long)na_uart0, ALTERA_UART_SIZE);
 	baud = CONFIG_SERIAL_ALTERA_UART_BAUDRATE;
 	baudclk = nasys_clock_freq / baud;
-	writew(baudclk, base + ALTERA_UART_DIVISOR_REG);
+	writew(baudclk, uartbase + ALTERA_UART_DIVISOR_REG);
 }
 
 #else
