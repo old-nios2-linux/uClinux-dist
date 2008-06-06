92e4e802da21559b1cff583da202bb81d31d12dc
Thomas Chou <thomas@wytron.com.tw>
2008-06-06 11:35:15 +0800

    altfb: add video pipeline support for NEEK
    The video pipeline of the LCD display on NEEK use SGDMA. This patch
    constructs the SGDMA descriptor lists and starts the memory to stream
    interface transfer.
    
    TODO: convert to platform driver model.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- linux-2.6.x/drivers/video/altfb.c
+++ linux-2.6.x/drivers/video/altfb.c
@@ -1,11 +1,11 @@
 /*
- *  altfb.c -- Altera VGA framebuffer driver
+ *  altfb.c -- Altera framebuffer driver
  * 
  *  Based on vfb.c -- Virtual frame buffer device
  *
  *	Copyright (C) 1997 Geert Uytterhoeven
  *      Copyright (C) 2002 James Simmons
- *	(C) Copyright 2008, Thomas Chou <thomas@wytron.com.tw>
+ *	Copyright (C) 2006-2008 Thomas Chou
  *
  *  This file is subject to the terms and conditions of the GNU General Public
  *  License. See the file COPYING in the main directory of this archive for
@@ -29,90 +29,193 @@
 #include <linux/fb.h>
 #include <linux/init.h>
 
-#define vgabase na_vga_controller_0
+#if defined(CONFIG_ALTERA_NEEK_C3)
+#define SGDMABASE na_lcd_sgdma	/* Altera Video Sync Generator */
+#define XRES 800
+#define YRES 480
+#define BPX  32
+#else
+#define VGABASE na_vga_controller_0	/* Altera VGA controller */
 #define XRES 640
 #define YRES 480
 #define BPX  16
+#endif
 
-    /*
-     *  RAM we reserve for the frame buffer. This defines the maximum screen
-     *  size
-     *
-     *  The default can be overridden if the driver is compiled as a module
-     */
+/*
+ *  RAM we reserve for the frame buffer. This defines the maximum screen
+ *  size
+ *
+ *  The default can be overridden if the driver is compiled as a module
+ */
 
 #define VIDEOMEMSIZE	(XRES * YRES * (BPX>>3))
 
 static struct fb_var_screeninfo altfb_default __initdata = {
-	.xres =		XRES,
-	.yres =		YRES,
-	.xres_virtual =	XRES,
-	.yres_virtual =	YRES,
+	.xres = XRES,
+	.yres = YRES,
+	.xres_virtual = XRES,
+	.yres_virtual = YRES,
 	.bits_per_pixel = BPX,
 #if (BPX == 16)
-	.red =		{ 11, 5, 0 },
-      	.green =	{ 5, 6, 0 },
-      	.blue =		{ 0, 5, 0 },
-#else  // BPX == 24
-	.red =		{ 16, 8, 0 },
-      	.green =	{ 8, 8, 0 },
-      	.blue =		{ 0, 8, 0 },
+	.red = {11, 5, 0},
+	.green = {5, 6, 0},
+	.blue = {0, 5, 0},
+#else /* BXP == 16 or BXP == 32 */
+	.red = {16, 8, 0},
+	.green = {8, 8, 0},
+	.blue = {0, 8, 0},
 #endif
-      	.activate =	FB_ACTIVATE_NOW,
-      	.height =	-1,
-      	.width =	-1,
-	// timing useless ?
-      	.pixclock =	20000,
-      	.left_margin =	64,
-      	.right_margin =	64,
-      	.upper_margin =	32,
-      	.lower_margin =	32,
-      	.hsync_len =	64,
-      	.vsync_len =	2,
-      	.vmode =	FB_VMODE_NONINTERLACED,
+	.activate = FB_ACTIVATE_NOW,
+	.height = -1,
+	.width = -1,
+	/* timing useless ? */
+	.pixclock = 20000,
+	.left_margin = 64,
+	.right_margin = 64,
+	.upper_margin = 32,
+	.lower_margin = 32,
+	.hsync_len = 64,
+	.vsync_len = 2,
+	.vmode = FB_VMODE_NONINTERLACED,
 };
 
 static struct fb_fix_screeninfo altfb_fix __initdata = {
-	.id =		"Altera FB",
-	.type =		FB_TYPE_PACKED_PIXELS,
-	.visual =	FB_VISUAL_TRUECOLOR,
-	.line_length =  (XRES * (BPX>>3)),
-	.xpanstep =	0,
-	.ypanstep =	0,
-	.ywrapstep =	0,
-	.accel =	FB_ACCEL_NONE,
+	.id = "Altera FB",
+	.type = FB_TYPE_PACKED_PIXELS,
+	.visual = FB_VISUAL_TRUECOLOR,
+	.line_length = (XRES * (BPX >> 3)),
+	.xpanstep = 0,
+	.ypanstep = 0,
+	.ywrapstep = 0,
+	.accel = FB_ACCEL_NONE,
 };
 
-static int altfb_mmap(struct fb_info *info,
-		    struct vm_area_struct *vma);
+static int altfb_mmap(struct fb_info *info, struct vm_area_struct *vma);
 
 static struct fb_ops altfb_ops = {
-	.fb_fillrect	= cfb_fillrect,
-	.fb_copyarea	= cfb_copyarea,
-	.fb_imageblit	= cfb_imageblit,
-	.fb_mmap	= altfb_mmap,
+	.fb_fillrect = cfb_fillrect,
+	.fb_copyarea = cfb_copyarea,
+	.fb_imageblit = cfb_imageblit,
+	.fb_mmap = altfb_mmap,
 };
-
 
 /* We implement our own mmap to set MAY_SHARE and add the correct size */
 static int altfb_mmap(struct fb_info *info, struct vm_area_struct *vma)
 {
-	vma->vm_flags |= VM_MAYSHARE| VM_SHARED;
+	vma->vm_flags |= VM_MAYSHARE | VM_SHARED;
 
 	vma->vm_start = info->fix.smem_start;
-	vma->vm_end   = info->fix.smem_start + info->fix.smem_len;
+	vma->vm_end = info->fix.smem_start + info->fix.smem_len;
 	return 0;
 }
 
-    /*
-     *  Initialisation
-     */
-
 static void altfb_platform_release(struct device *device)
 {
-	// This is called when the reference count goes to zero.
-	dev_err(device, "This driver is broken, please bug the authors so they will fix it.\n");
+	/* This is called when the reference count goes to zero. */
+	dev_err(device,
+		"This driver is broken, please bug the authors so they will fix it.\n");
 }
+
+/*
+ *  Initialization
+ */
+
+#ifdef SGDMABASE
+
+#define ALTERA_SGDMA_IO_EXTENT 0x400
+
+#define ALTERA_SGDMA_STATUS 0
+#define ALTERA_SGDMA_STATUS_BUSY_MSK (0x10)
+
+#define ALTERA_SGDMA_CONTROL 16
+#define ALTERA_SGDMA_CONTROL_RUN_MSK  (0x20)
+#define ALTERA_SGDMA_CONTROL_SOFTWARERESET_MSK (0X10000)
+#define ALTERA_SGDMA_CONTROL_PARK_MSK (0X20000)
+
+#define ALTERA_SGDMA_NEXT_DESC_POINTER 32
+
+/* SGDMA can only transfer this many bytes per descriptor */
+#define DISPLAY_BYTES_PER_DESC 0xFF00
+#define ALTERA_SGDMA_DESCRIPTOR_CONTROL_GENERATE_EOP_MSK (0x1)
+#define ALTERA_SGDMA_DESCRIPTOR_CONTROL_GENERATE_SOP_MSK (0x4)
+#define ALTERA_SGDMA_DESCRIPTOR_CONTROL_OWNED_BY_HW_MSK (0x80)
+
+typedef struct {
+	u32 *read_addr;
+	u32 read_addr_pad;
+
+	u32 *write_addr;
+	u32 write_addr_pad;
+
+	u32 *next;
+	u32 next_pad;
+
+	u16 bytes_to_transfer;
+	u8 read_burst;
+	u8 write_burst;
+
+	u16 actual_bytes_transferred;
+	u8 status;
+	u8 control;
+
+} __attribute__ ((packed)) sgdma_desc;
+
+static int altfb_dma_start(unsigned long start, unsigned long len)
+{
+	unsigned long base =
+	    (unsigned long)ioremap(SGDMABASE, ALTERA_SGDMA_IO_EXTENT);
+	sgdma_desc *desc, *desc1;
+	int ndesc = (len + DISPLAY_BYTES_PER_DESC - 1) / DISPLAY_BYTES_PER_DESC;
+	int ndesc_size = sizeof(sgdma_desc) * ndesc;
+	int i;
+
+	writel(ALTERA_SGDMA_CONTROL_SOFTWARERESET_MSK,
+	       base + ALTERA_SGDMA_CONTROL);	/* halt current transfer */
+	writel(0, base + ALTERA_SGDMA_CONTROL);	/* disable interrupts */
+	writel(0xff, base + ALTERA_SGDMA_STATUS);	/* clear status */
+
+	/* assume cache line size is 32, which is required by sgdma desc */
+	desc1 = kzalloc(ndesc_size, GFP_KERNEL);
+	if (desc1 == NULL)
+		return -ENOMEM;
+	desc1 = ioremap((unsigned long)desc1, ndesc_size);
+
+	for (i = 0, desc = desc1; i < ndesc; i++, desc++) {
+		unsigned ctrl = ALTERA_SGDMA_DESCRIPTOR_CONTROL_OWNED_BY_HW_MSK;
+		desc->read_addr = (void *)start;
+		if (i == (ndesc - 1)) {
+			desc->next = (void *)desc1;
+			desc->bytes_to_transfer = len;
+			ctrl |=
+			    ALTERA_SGDMA_DESCRIPTOR_CONTROL_GENERATE_EOP_MSK;
+		} else {
+			desc->next = (void *)(desc + 1);
+			desc->bytes_to_transfer = DISPLAY_BYTES_PER_DESC;
+		}
+		if (i == 0)
+			ctrl |=
+			    ALTERA_SGDMA_DESCRIPTOR_CONTROL_GENERATE_SOP_MSK;
+		desc->control = ctrl;
+		start += DISPLAY_BYTES_PER_DESC;
+		len -= DISPLAY_BYTES_PER_DESC;
+	}
+
+	writel((unsigned long)desc1, base + ALTERA_SGDMA_NEXT_DESC_POINTER);
+	writel(ALTERA_SGDMA_CONTROL_RUN_MSK | ALTERA_SGDMA_CONTROL_PARK_MSK,
+	       base + ALTERA_SGDMA_CONTROL);	/* start */
+	return 0;
+}
+#else
+static int altfb_dma_start(unsigned long start, unsigned long len)
+{
+	unsigned long base = ioremap(VGABASE, 16);
+	writel(0x0, base + 0);	/* Reset the VGA controller */
+	writel(start, base + 4);	/* Where our frame buffer starts */
+	writel(len, base + 8);	/* buffer size */
+	writel(0x1, base + 0);	/* Set the go bit */
+	return 0;
+}
+#endif
 
 static int __init altfb_probe(struct platform_device *dev)
 {
@@ -120,10 +223,11 @@ static int __init altfb_probe(struct platform_device *dev)
 	int retval = -ENOMEM;
 
 	altfb_fix.smem_len = VIDEOMEMSIZE;
-	altfb_fix.smem_start = __get_free_pages(GFP_KERNEL, get_order(altfb_fix.smem_len));
+	altfb_fix.smem_start =
+	    (unsigned long)kzalloc(altfb_fix.smem_len, GFP_KERNEL);
 	if (!altfb_fix.smem_start) {
-		printk("Unable to allocate %d PAGEs(%d Bytes) fb memory\n",
-		       get_order(altfb_fix.smem_len), altfb_fix.smem_len);
+		printk("Unable to allocate %d Bytes fb memory\n",
+		       altfb_fix.smem_len);
 		return -ENOMEM;
 	}
 
@@ -148,21 +252,19 @@ static int __init altfb_probe(struct platform_device *dev)
 		goto err2;
 	platform_set_drvdata(dev, info);
 
-	outl(0x0,vgabase+0);  // Reset the VGA controller
-	outl(altfb_fix.smem_start, vgabase+4);  // Where our frame buffer starts
-	outl(altfb_fix.smem_len, vgabase+8);  // amount of memory needed
-	outl(0x1, vgabase+0);  // Set the go bit
+	if (altfb_dma_start(altfb_fix.smem_start, altfb_fix.smem_len))
+		goto err2;
 
-	printk(KERN_INFO "fb%d: %s frame buffer device\n", info->node, info->fix.id);
-	// printk("vga %08x, video %08x+%08x\n",vgabase,videomemory,videomemorysize);
+	printk(KERN_INFO "fb%d: %s frame buffer device\n", info->node,
+	       info->fix.id);
 	return 0;
-err2:
+      err2:
 	fb_dealloc_cmap(&info->cmap);
-err1:
+      err1:
 	iounmap((void *)altfb_fix.smem_start);
 	framebuffer_release(info);
-err:
-	free_pages(altfb_fix.smem_start, get_order(altfb_fix.smem_len));
+      err:
+	kfree((void *)altfb_fix.smem_start);
 	return retval;
 }
 
@@ -180,19 +282,19 @@ static int altfb_remove(struct platform_device *dev)
 }
 
 static struct platform_driver altfb_driver = {
-	.probe	= altfb_probe,
+	.probe = altfb_probe,
 	.remove = altfb_remove,
 	.driver = {
-		.name	= "altfb",
-	},
+		   .name = "altfb",
+		   },
 };
 
 static struct platform_device altfb_device = {
-	.name	= "altfb",
-	.id	= 0,
-	.dev	= {
+	.name = "altfb",
+	.id = 0,
+	.dev = {
 		.release = altfb_platform_release,
-	}
+		}
 };
 
 static int __init altfb_init(void)
@@ -221,4 +323,4 @@ static void __exit altfb_exit(void)
 module_exit(altfb_exit);
 
 MODULE_LICENSE("GPL");
-#endif				/* MODULE */
+#endif /* MODULE */
