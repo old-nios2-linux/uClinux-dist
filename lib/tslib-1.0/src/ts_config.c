9d69a89aeae63123a6ec303195f8256c533591dc
Thomas Chou <thomas@wytron.com.tw>
2008-06-10 08:27:23 +0800

    tslib: static config table in ts_config
    The patch replaced the dynamic module loading with a config table.
    The /etc/ts.conf was not used.
    
    The configure scripts were rebuilt, with
    aclocal
    automake
    autoconf
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- lib/tslib-1.0/src/ts_config.c
+++ lib/tslib-1.0/src/ts_config.c
@@ -18,83 +18,46 @@
 
 #include "tslib-private.h"
 
-/* Maximum line size is BUF_SIZE - 2 
- * -1 for fgets and -1 to test end of line
- */
-#define BUF_SIZE 512
+struct module_list {
+  TSAPI struct tslib_module_info * (*mod_init)(struct tsdev *dev, const char *params);
+	char *params;
+};
+
+/* available modules */
+TSAPI struct tslib_module_info *input_mod_init(struct tsdev *dev, const char *params);
+TSAPI struct tslib_module_info *pthres_mod_init(struct tsdev *dev, const char *params);
+TSAPI struct tslib_module_info *variance_mod_init(struct tsdev *dev, const char *params);
+TSAPI struct tslib_module_info *linear_mod_init(struct tsdev *dev, const char *params);
+
+/* Put your own list here, to replace ts.conf */
+/* first one must be raw */
+static struct module_list config_list[] = {
+	{ input_mod_init, NULL },
+	{ pthres_mod_init, "pmin=1" },
+	{ variance_mod_init, "delta=30" },
+	{ linear_mod_init, NULL },
+};
+
+#define NR_CONF (sizeof(config_list) / sizeof(config_list[0]))
 
 int ts_config(struct tsdev *ts)
 {
-	char buf[BUF_SIZE], *p;
-	FILE *f;
-	int line = 0;
 	int ret = 0;
+	int i;
 
-	char *conffile;
-
-	if( (conffile = getenv("TSLIB_CONFFILE")) == NULL) {
-		conffile = strdup (TS_CONF);
-	}
-
-	f = fopen(conffile, "r");
-	if (!f) {
-		perror("Couldnt open tslib config file");
-		return -1;
-	}
-
-	buf[BUF_SIZE - 2] = '\0';
-	while ((p = fgets(buf, BUF_SIZE, f)) != NULL) {
-		char *e;
-		char *tok;
-		char *module_name;
-
-		line++;
-
-		/* Chomp */
-		e = strchr(p, '\n');
-		if (e) {
-			*e = '\0';
-		}
-
-		/* Did we read a whole line? */
-		if (buf[BUF_SIZE - 2] != '\0') {
-			ts_error("%s: line %d too long\n", conffile, line);
-			break;
-		}
-
-		tok = strsep(&p, " \t");
-		
-		/* Ignore comments or blank lines.
-		 * Note: strsep modifies p (see man strsep)
-		 */
-
-		if (p==NULL || *tok == '#')
-			continue;
-
-		/* Search for the option. */
-		if (strcasecmp(tok, "module") == 0) {
-			module_name = strsep(&p, " \t");
-			ret = ts_load_module(ts, module_name, p);
-		}
-		else if (strcasecmp(tok, "module_raw") == 0) {
-			module_name = strsep(&p, " \t");
-			ret = ts_load_module_raw(ts, module_name, p);
+	for (i = 0; i< NR_CONF; i++) {
+		struct tslib_module_info *info;
+		info = config_list[i].mod_init(ts, config_list[i].params); 
+		if (i == 0) {
+			ret = __ts_attach_raw(ts, info);
 		} else {
-			ts_error("%s: Unrecognised option %s:%d:%s\n", conffile, line, tok);
-			break;
+			ret = __ts_attach(ts, info);
 		}
 		if (ret != 0) {
-			ts_error("Couldnt load module %s\n", module_name);
+			ts_error("Couldnt load module %d\n", i);
 			break;
 		}
 	}
-
-	if (ts->list_raw == NULL) {
-		ts_error("No raw modules loaded.\n");
-		ret = -1;
-	}
-
-	fclose(f);
 
 	return ret;
 }
