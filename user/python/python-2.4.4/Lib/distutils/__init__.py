3a4b97c5c18ed85631ef392a49b0e3ad465c98c5
Thomas Chou <thomas@wytron.com.tw>
2008-07-23 08:35:51 +0800

    python: merge python-2.4.4 from blackfin uclinux
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

---
+++ user/python/python-2.4.4/Lib/distutils/__init__.py
@@ -0,0 +1,15 @@
+"""distutils
+
+The main package for the Python Module Distribution Utilities.  Normally
+used from a setup script as
+
+   from distutils.core import setup
+
+   setup (...)
+"""
+
+# This module should be kept compatible with Python 2.1.
+
+__revision__ = "$Id: __init__.py 4802 2007-01-23 21:26:03Z vapier $"
+
+__version__ = "2.4.4"
