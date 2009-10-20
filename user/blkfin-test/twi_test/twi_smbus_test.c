6bafd4e4af380c46dd27fabcfa20f93904cfcdc1
Sonic Zhang <sonic.zhang@analog.com>
2009-10-20 04:43:49 +0000

    Fix bug[#5620] Update i2c device probe method in twi test module.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@9010 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/twi_test/twi_smbus_test.c
+++ user/blkfin-test/twi_test/twi_smbus_test.c
@@ -5,44 +5,27 @@
 /* AD5280 vcomm */
 static unsigned char vcomm_value = 150;
 
-static char ad5280_drv_name[] = "ad5280";
-static struct i2c_driver ad5280_driver;
+static char ad5280_drv_name[] = "bf537-lq035-ad5280";
 
-static unsigned short ignore[] 		= { I2C_CLIENT_END };
-static unsigned short normal_addr[] = { 0x58>>1, I2C_CLIENT_END };
-
-static struct i2c_client_address_data addr_data = {
-	.normal_i2c			= normal_addr,
-	.probe				= normal_addr,
-	.ignore				= ignore,
-};
-
-static int ad5280_probe(struct i2c_adapter *adap, int addr, int kind)
+static int __devinit ad5280_probe(struct i2c_client *client,
+					const struct i2c_device_id *id)
 {
-	struct i2c_client client;
-	int rc;
+	int ret;
 	unsigned char new_vcomm=0;
 
-	memset(&client, 0, sizeof(struct i2c_client));
-	strncpy(client.name, ad5280_drv_name, I2C_NAME_SIZE);
-	client.addr = addr;
-	client.adapter = adap;
-	client.driver = &ad5280_driver;
-
-	if ((rc = i2c_attach_client(&client)) != 0) {
-		printk("TWI_SMBUS_TEST: i2c_attach_client fail: %d\n", rc);
-		return rc;
+	if (!i2c_check_functionality(client->adapter,
+					I2C_FUNC_SMBUS_BYTE_DATA)) {
+		dev_err(&client->dev, "SMBUS Byte Data not Supported\n");
+		return -EIO;
 	}
 
-	rc = i2c_smbus_write_byte_data(&client, 0x00, vcomm_value);
-	if(rc) {
-		i2c_detach_client(&client);
+	ret = i2c_smbus_write_byte_data(&client, 0x00, vcomm_value);
+	if(ret) {
 		printk("TWI_SMBUS_TEST: i2c_smbus_write_byte_data fail: %d\n", rc);
-		return -1;
+		return ret;
 	}
 
 	new_vcomm = i2c_smbus_read_byte_data(&client, 0x00);
-	i2c_detach_client(&client);
 	if(new_vcomm != vcomm_value) {
 		printk("TWI_SMBUS_TEST: i2c_smbus_read_byte_data fails: %d\n", new_vcomm);
 		return -1;
@@ -52,29 +35,23 @@ static int ad5280_probe(struct i2c_adapter *adap, int addr, int kind)
 	return 0;
 }
 
-static int ad5280_attach(struct i2c_adapter *adap)
-{
-	if (adap->algo->functionality)
-		return i2c_probe(adap, &addr_data, ad5280_probe);
-	else
-		return ad5280_probe(adap, 0x58>>1, 0);
-}
+static const struct i2c_device_id ad5280_id[] = {
+	{ad5280_drv_name, 0},
+	{}
+};
 
-static int ad5280_dettach(struct i2c_adapter *adap) {
-	return 0;
-}
+MODULE_DEVICE_TABLE(i2c, ad5280_id);
 
 static struct i2c_driver ad5280_driver = {
-	.id              = 0x65,
-	.attach_adapter  = ad5280_attach,
-	.detach_adapter  = ad5280_dettach,
 	.driver		= {
 		.name	= ad5280_drv_name,
 		.owner	= THIS_MODULE,
 	},
+	.probe = ad5280_probe,
+	.id_table = ad5280_id,
 };
 
-static int test_init(void)
+static int __init twi_test_init(void)
 {
 
 	printk("TWI smbus api test\n");
@@ -83,12 +60,12 @@ static int test_init(void)
 	return 0;
 }
 
-static void test_exit(void)
+static void __exit twi_test_exit(void)
 {
 	i2c_del_driver(&ad5280_driver);
 }
 
 MODULE_LICENSE("GPL");
 
-module_init(test_init);
-module_exit(test_exit);
+module_init(twi_test_init);
+module_exit(twi_test_exit);
