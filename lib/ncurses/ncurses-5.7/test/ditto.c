fc41e0eef9ec4f6b498741aebe400402a4b0f101
Graf Yang <graf.yang@analog.com>
2009-11-04 07:20:05 +0000

    Fix ncurses-5.7 compile error which caused by lacking of fork stub in current toolchain
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@9099 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- lib/ncurses/ncurses-5.7/test/ditto.c
+++ lib/ncurses/ncurses-5.7/test/ditto.c
@@ -162,7 +162,11 @@ open_tty(char *path)
 	failed(slave_name);
     }
     sprintf(s_option, "-S%s/%d", slave_name, aslave);
+#ifdef __uClinux__
+    if (vfork()) {
+#else
     if (fork()) {
+#endif
 	execlp("xterm", "xterm", s_option, "-title", path, (char *) 0);
 	_exit(0);
     }
