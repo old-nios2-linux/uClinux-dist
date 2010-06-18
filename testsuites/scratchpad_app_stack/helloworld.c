04610826188c8b83ff1da41b98ef335e40f6b0b9
Barry Song <barry.song@analog.com>
2010-06-18 08:20:01 +0000

    bug [#6034], improve L1 stack test to match argv
    argv is placed in L1 stack too, this patch makes the test more useful.
    
    
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@9682 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/scratchpad_app_stack/helloworld.c
+++ testsuites/scratchpad_app_stack/helloworld.c
@@ -4,17 +4,19 @@
 #define TEST_SCRATCHPAD_HIGH     0xffb01000
 
 
-int main()
+int main(int argc, char *argv[])
 {
         char *str;
         str="Hello world!";
 	printf("%s, %p\n", str,&str);
 
-        if (((unsigned long)&str > TEST_SCRATCHPAD_HIGH) || ((unsigned long)&str < TEST_SCRATCHPAD_LOW)) {
-                printf("        TEST FAIL\n");
-        } else {
-                printf("        TEST PASS\n");
-        }
-
+	if (((unsigned long)&str > TEST_SCRATCHPAD_HIGH) || ((unsigned long)&str < TEST_SCRATCHPAD_LOW)) {
+		printf("        TEST FAIL, stack address not in L1\n");
+	} else {
+		if (strstr(argv[0], "helloworld"))
+			printf("        TEST PASS, %s\n", argv[0]);
+		else
+			printf("        TEST FAIL, argv is wrong\n");
+	}
 }
 
