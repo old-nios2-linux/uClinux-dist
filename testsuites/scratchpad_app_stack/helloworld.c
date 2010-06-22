3cf843ee98cc57e4bc20952db293a7a90fe87971
Mike Frysinger <vapier@gentoo.org>
2010-06-22 04:42:21 +0000

    scratchpad_app_stack: add a few comments
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@9687 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/scratchpad_app_stack/helloworld.c
+++ testsuites/scratchpad_app_stack/helloworld.c
@@ -53,15 +53,19 @@ int main(int argc, char *argv[])
 {
 	char *str = "Hello world!";
 
+	/* str variable should be in L1, but not the string */
 	check_string(str, 1, 0);
 	puts("");
 
+	/* argv variable and the array should be in L1, but not the strings */
 	check_pointer(argv, 1, 1);
 	check_string(argv[0], 1, 0);
 	if (!strstr(argv[0], "a.out") && !strstr(argv[0], "helloworld"))
 		puts("FAIL:  test app should be named 'helloworld'");
 	puts("");
 
+	/* the pointers should be in L1, but not the environ symbol or
+	 * the strings themselves */
 	check_pointer(environ, 0, 1);
 	check_string(environ[0], 1, 0);
 	puts("");
