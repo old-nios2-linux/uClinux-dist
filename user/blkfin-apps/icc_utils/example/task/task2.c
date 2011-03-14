eaa00c2de3522aaffcde195d590264c8412676e6
steven miao <realmz6@gmail.com>
2011-03-14 07:08:05 +0000

    [#4788] bf561 inter-core communication : put coreb task into ICC_MEM define ICC_MEM memory description put coreb task into ICC_MEM
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10121 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/icc_utils/example/task/task2.c
+++ user/blkfin-apps/icc_utils/example/task/task2.c
@@ -1,14 +1,14 @@
 #include <icc.h>
 #include <protocol.h>
 
-#define LOCAL_SESSION 5
-int default_session_handle(struct sm_message *msg, struct sm_session *session);
+#define LOCAL_SESSION 6
+int default_session_handle(struct sm_message *message, struct sm_session *session);
 
-sm_uint32_t index;
-void icc_task_init(int argc, char *argv[])
+sm_uint32_t __icc_task_data index;
+void  __icc_task icc_task_init(int argc, char *argv[])
 {
 	struct sm_session *session;
-	index = sm_create_session(LOCAL_SESSION, SP_PACKET);
+	index = sm_create_session(LOCAL_SESSION, SP_SESSION_PACKET);
 	coreb_msg("%s() %s %s index %d\n", __func__, argv[0], argv[1], index);
 	if (index >= 32)
 		coreb_msg("create session failed\n");
@@ -18,18 +18,20 @@ void icc_task_init(int argc, char *argv[])
 	coreb_msg("%s() end\n", __func__);
 }
 
-void icc_task_exit(void)
+void  __icc_task icc_task_exit(void)
 {
-	sm_destroy_session(index);
+	sm_destroy_session(LOCAL_SESSION);
 }
 
-int default_session_handle(struct sm_message *msg, struct sm_session *session)
+int  __icc_task default_session_handle(struct sm_message *message, struct sm_session *session)
 {
 	void *buf;
+	struct l3_proto_head *p;
 	sm_uint32_t len;
 	int ret;
+	struct sm_msg *msg = &message->msg;
 	coreb_msg(" %s session %d msg %s \n",__func__, session->local_ep, msg->payload);
-	coreb_msg("dst %d dstep %d, src %d, srcep %d\n", msg->dst, msg->dst_ep, msg->src, msg->src_ep);
+	coreb_msg("dst %d dstep %d, src %d, srcep %d\n", message->dst, msg->dst_ep, message->src, msg->src_ep);
 
 	ret = sm_recv_packet(index, &buf, len);
 	if (ret <= 0) {
@@ -38,16 +40,23 @@ int default_session_handle(struct sm_message *msg, struct sm_session *session)
 	}
 	/* handle payload */
 	coreb_msg("processing msg %s\n", buf);
-	if (*(char *)buf == '0') {
-		int len = 64;
+	p = (struct l3_proto_head *)buf;
+	if (p->type == L3_TYPE_AUDIO) {
+		int len = sizeof(struct l3_proto_head);
+		unsigned int audio_buf = p->chunk_addr;
+		unsigned int size = p->chunk_size;
 		int dst_ep = msg->src_ep;
-		int dst_cpu = msg->src;
+		int dst_cpu = message->src;
 		void *send_buf = sm_send_request(len, session);
 		coreb_msg("coreb send buf %x\n", send_buf);
 		if (!send_buf)
 			coreb_msg("NO MEM\n");
 		memset(send_buf, 0, len);
-		*(char *)send_buf = 'f';
+		p = send_buf;
+		p->type = L3_TYPE_AUDIO;
+		p->chunk_addr = audio_buf;
+		p->chunk_size = size;
+		p->status = 1;
 		sm_send_packet(index, dst_ep, dst_cpu, send_buf, len);
 	} else {
 		coreb_msg("msg payload %s \n", buf);
@@ -57,5 +66,4 @@ int default_session_handle(struct sm_message *msg, struct sm_session *session)
 
 	return 0;
 }
-
 
--- user/blkfin-apps/icc_utils/include/protocol.h
+++ user/blkfin-apps/icc_utils/include/protocol.h
@@ -6,6 +6,7 @@
 struct coreb_icc_node {
 	struct sm_icc_desc icc_info;
 	struct gen_pool *pool;
+	struct gen_pool *msg_pool;
 };
 
 extern struct coreb_icc_node coreb_info;
