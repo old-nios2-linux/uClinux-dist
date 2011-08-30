15b7fc9b3ee76cd1e119df8378f78ad6366bbb6f
Mingquan Pan <grace.pan@analog.com>
2011-08-30 05:44:32 +0000

    [#6579] Add mcapi case: for message and packet running with both processes on.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10337 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ lib/libmcapi/libmcapi-2.0/tests/msg2.c
@@ -0,0 +1,231 @@
+/* Test: msg2
+   Description: Tests msg send/recv under two processes situation..  
+   It tests all error conditions.  
+*/
+
+#include <mcapi.h>
+#include <mca.h>
+#include <mcapi_test.h>
+#include <stdio.h>
+#include <stdlib.h> /* for malloc */
+#include <string.h>
+#include <mcapi_impl_spec.h>
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define NUM_SIZES 4
+#define BUFF_SIZE 64
+#define DOMAIN 0
+#define NODE 0
+
+
+#define WRONG wrong(__LINE__);
+void wrong(unsigned line)
+{
+  fprintf(stderr,"WRONG: line=%u\n", line);
+  fflush(stdout);
+  exit(1);
+}
+
+void send (mcapi_endpoint_t send, mcapi_endpoint_t recv,char* msg,mcapi_status_t status,int exp_status) {
+  int size = strlen(msg);
+  int priority = 1;
+  mcapi_request_t request;
+
+  mcapi_msg_send_i(send,recv,msg,size,priority,&request,&status);
+  if (status != exp_status) { WRONG}
+  if (status == MCAPI_SUCCESS) {
+    printf("endpoint=%i has sent: [%s]\n",(int)send,msg);
+  }
+}
+
+void recv (mcapi_endpoint_t recv,mcapi_status_t status,int exp_status) {
+  size_t recv_size;
+  char buffer[BUFF_SIZE];
+  mcapi_request_t request;
+  mcapi_msg_recv_i(recv,buffer,BUFF_SIZE,&request,&status);
+  if (status != exp_status) { WRONG}
+  if (status == MCAPI_SUCCESS) {
+    printf("endpoint=%i has received: [%s]\n",(int)recv,buffer);
+  }
+}
+
+void do_child()
+{
+	mcapi_status_t status;
+	mcapi_param_t parms;
+	mcapi_info_t version;
+	mcapi_request_t request;
+	mcapi_endpoint_t ep1,ep2,ep3,ep4;
+
+	mcapi_uint_t avail;
+	int s;
+	int i;
+	int fd;
+
+	fd = open("/tmp/child.log", O_CREAT | O_RDWR, 0666);
+	if (fd < 0)
+		{ WRONG };
+
+	if (dup2(fd, STDOUT_FILENO) == -1) {
+		WRONG;
+	}
+	close(fd);
+
+	printf("test log\n");
+
+      	mcapi_initialize(DOMAIN,NODE,NULL,&parms,&version,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+          
+        /* create endpoints */
+        ep2 = mcapi_endpoint_create(MASTER_PORT_NUM2,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+        printf("ep2 %x   \n", ep1);
+     
+        ep4 = mcapi_endpoint_get (DOMAIN,SLAVE_NODE_NUM,SLAVE_PORT_NUM2,MCA_INFINITE,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+        printf("ep4 %x   \n", ep2);
+     
+        /* send and recv messages on the endpoints */
+        /* regular endpoints */
+     
+        for (s = 0; s < NUM_SIZES; s++) {
+     
+        send (ep1,ep2,"Child Hello MCAPI",status,MCAPI_SUCCESS);
+        if (status != MCAPI_SUCCESS) { WRONG }
+        printf("coreA: The %d time sending, status %d\n",s, status);
+     
+        }
+
+        mcapi_endpoint_delete(ep2,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+
+	mcapi_finalize(&status);
+
+	printf("Child   Test PASSED\n");
+
+        _exit(0);
+}
+
+void do_parent()
+{
+	mcapi_status_t status;
+	mcapi_param_t parms;
+	mcapi_info_t version;
+	mcapi_request_t request;
+	mcapi_endpoint_t ep1,ep2,ep3,ep4;
+	mcapi_boolean_t rc = MCAPI_FALSE;
+
+	mcapi_uint_t avail;
+	int s;
+	int i;
+
+      	mcapi_initialize(DOMAIN,NODE,NULL,&parms,&version,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+          
+        /* create endpoints */
+        ep1 = mcapi_endpoint_create(MASTER_PORT_NUM1,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+        printf("ep1 %x   \n", ep1);
+     
+        ep3 = mcapi_endpoint_get (DOMAIN,SLAVE_NODE_NUM,SLAVE_PORT_NUM1,MCA_INFINITE,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+        printf("ep3 %x   \n", ep2);
+     
+        /* send and recv messages on the endpoints */
+        /* regular endpoints */
+     
+        for (s = 0; s < NUM_SIZES; s++) {
+     
+        send (ep1,ep3,"Parent Hello MCAPI",status,MCAPI_SUCCESS);
+        if (status != MCAPI_SUCCESS) { WRONG }
+        printf("coreA: The %d time sending, status %d\n",s, status);
+     
+        }
+
+#if 0
+  rc = 0;
+  while (1) {
+	avail = mcapi_msg_available(ep1, &status);
+	if (avail > 0) {
+		recv (ep1,status,MCAPI_SUCCESS);
+  		if (status != MCAPI_SUCCESS) { WRONG }
+                printf("CoreA : message recv. The %d time receiving ok, status %i\n", rc, status);
+
+		if (rc == (NUM_SIZES - 1))
+                	break;
+                rc++;
+	}
+	sleep(2);
+  }
+#endif
+        mcapi_endpoint_delete(ep1,&status);
+        if (status != MCAPI_SUCCESS) { WRONG }
+
+
+	mcapi_finalize(&status);
+
+}
+
+int main (int ac, char **av) {
+	mcapi_status_t status;
+	mcapi_param_t parms;
+	mcapi_info_t version;
+	mcapi_request_t request;
+	mcapi_endpoint_t ep1,ep2,ep3,ep4;
+
+	/* cases:
+	1: both named endpoints (1,2)
+	*/
+	mcapi_sclchan_send_hndl_t s1;
+	mcapi_sclchan_recv_hndl_t r1;
+	mcapi_uint_t avail;
+	int s;
+	int i;
+	mcapi_boolean_t rc = MCAPI_FALSE;
+
+	int parentpid, childpid;
+	char opt;
+
+	while ( (opt = getopt(ac, av, "C")) > 0) {
+
+ 	switch (opt) {
+                case 'C': /* Run child */
+			do_child();
+                        break;
+                default:
+			break;
+
+ 	}
+	}
+
+	childpid = vfork();
+	if (childpid < 0) {
+		WRONG
+	}
+	if (childpid == 0) {/* child */
+
+        return	execlp(av[0], av[0], "-C", (char *) NULL);
+
+	} else {/* parent */
+ 
+	int stat_val;
+
+	do_parent();	
+
+        waitpid(childpid, &stat_val, 0);
+	if (WIFEXITED(stat_val))
+		 printf("Child exited with code %d\n",WEXITSTATUS(stat_val));
+	else if (WIFSIGNALED(stat_val))
+		printf("Child terminated  abnormally, signal %d\n", WTERMSIG(stat_val));
+
+	printf("Parent   Test PASSED\n");
+
+	}		
+
+	return 0;
+}
