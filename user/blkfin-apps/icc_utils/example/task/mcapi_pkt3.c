52cc96bc956a81c6212bb55033ac6d902d0e5a88
Mingquan Pan <grace.pan@analog.com>
2011-09-20 08:49:30 +0000

    [#6579] Update mcapi case: Refine pkt3.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10408 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/icc_utils/example/task/mcapi_pkt3.c
+++ user/blkfin-apps/icc_utils/example/task/mcapi_pkt3.c
@@ -11,12 +11,12 @@
 #include <debug.h>
 
 #define NUM_SIZES 2
-#define BUFF_SIZE 128*1024
+#define RECV_BUFF_SIZE 128*1024
+#define SEND_BUFF_SIZE 128
 
 #define DOMAIN 0
 
-char buffer[BUFF_SIZE] = "mcapi_pkt response";
-void *pbuffer = NULL;
+char buffer[SEND_BUFF_SIZE] = "mcapi_pkt response";
 
 #define WRONG wrong(__LINE__);
 void wrong(unsigned line)
@@ -32,6 +32,7 @@ void recv_pktchan(mcapi_endpoint_t recv,mcapi_status_t status,int exp_status)
 	mcapi_endpoint_t send_back;
 	mcapi_status_t status1;
 	mcapi_pktchan_recv_hndl_t r1;
+	char *pbuffer = NULL;
 
 	mcapi_pktchan_recv_open_i(&r1,recv, &request1, &status);
 	coreb_msg("open recv chan status %x   \n", status);
@@ -39,7 +40,9 @@ void recv_pktchan(mcapi_endpoint_t recv,mcapi_status_t status,int exp_status)
 	mcapi_pktchan_recv_i(r1,(void **)&pbuffer,&request1,&status);
 	if (status != exp_status) { WRONG}
 	if (status == MCAPI_SUCCESS) {
-		coreb_msg("endpoint=%i has received: [%s]\n",(int)recv,pbuffer);
+		coreb_msg("endpoint=%i has received!! \n",(int)recv);
+
+		*pbuffer=*pbuffer + 1;
 		if (pbuffer)
 			mcapi_pktchan_release(pbuffer, &status1);
 		mcapi_pktchan_recv_close_i(r1,&request1, &status1);
@@ -52,7 +55,6 @@ void send_pktchan(mcapi_endpoint_t send,mcapi_status_t status,int exp_status)
 	mcapi_request_t request2;
 	mcapi_endpoint_t send_back;
 	mcapi_pktchan_send_hndl_t s1;
-	void *pbuffer = NULL;
 	size_t send_size;
 
 	/*************************** open the channels *********************/
@@ -61,7 +63,7 @@ void send_pktchan(mcapi_endpoint_t send,mcapi_status_t status,int exp_status)
 	mcapi_pktchan_send_open_i(&s1,send, &request1, &status);
 	coreb_msg("open send chan status %x   \n", status);
 
-	mcapi_pktchan_send_i(s1,pbuffer,BUFF_SIZE,&request1,&status);
+	mcapi_pktchan_send_i(s1,buffer,SEND_BUFF_SIZE,&request1,&status);
 	if (status != exp_status) { WRONG}
 	if (status == MCAPI_SUCCESS) {
 		coreb_msg("endpoint=%i has sent: [%s]\n",(int)send,buffer);
@@ -124,8 +126,9 @@ void icc_task_init(int argc, char *argv[])
 
 
 	                i++;
-      			if ( i == NUM_SIZES*2 ) {
+      			if ( i == NUM_SIZES*2 - 1 ) {
 				send_pktchan(ep1,status,MCAPI_SUCCESS);
+				send_pktchan(ep2,status,MCAPI_SUCCESS);
 			}
 		}
 	}
@@ -134,7 +137,7 @@ void icc_task_init(int argc, char *argv[])
 	mcapi_endpoint_delete(ep2,&status);
 
 	mcapi_finalize(&status);
-	if ((pass_num1 + pass_num2) == NUM_SIZES *2)
+	if ((pass_num1 + pass_num2) == NUM_SIZES *2 - 1 )
 	coreb_msg("CoreB Test PASSED\n");
   	else
   	coreb_msg("CoreB Test FAILED\n");
