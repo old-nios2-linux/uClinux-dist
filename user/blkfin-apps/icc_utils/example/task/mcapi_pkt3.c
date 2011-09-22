79f5ce0e7960f2bc9f090015f1f46741df42fe58
steven miao <realmz6@gmail.com>
2011-09-22 02:14:50 +0000

    libmcapi_coreb: task mcapi_pkt3: parse message header to get indirect buffer
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10412 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/icc_utils/example/task/mcapi_pkt3.c
+++ user/blkfin-apps/icc_utils/example/task/mcapi_pkt3.c
@@ -16,6 +16,14 @@
 
 #define DOMAIN 0
 
+struct message_hdr {
+	uint32_t magic;
+	uint32_t vaddr;
+	uint32_t paddr;
+	uint32_t size;
+	uint32_t op;
+};
+
 char buffer[SEND_BUFF_SIZE] = "mcapi_pkt response";
 
 #define WRONG wrong(__LINE__);
@@ -24,7 +32,7 @@ void wrong(unsigned line)
 	coreb_msg("WRONG: line==%i \n",line);
 }
 
-void recv_pktchan(mcapi_endpoint_t recv,mcapi_status_t status,int exp_status)
+void recv_pktchan(mcapi_endpoint_t recv,mcapi_status_t *status)
 {
 	size_t recv_size;
 	mcapi_request_t request1;
@@ -33,16 +41,35 @@ void recv_pktchan(mcapi_endpoint_t recv,mcapi_status_t status,int exp_status)
 	mcapi_status_t status1;
 	mcapi_pktchan_recv_hndl_t r1;
 	char *pbuffer = NULL;
+	struct message_hdr *ph;
+	uint32_t vaddr, paddr, size, op;
+	char *p;
+	int i;
 
-	mcapi_pktchan_recv_open_i(&r1,recv, &request1, &status);
-	coreb_msg("open recv chan status %x   \n", status);
+	mcapi_pktchan_recv_open_i(&r1,recv, &request1, status);
+	coreb_msg("open recv chan status %x   \n", *status);
 
-	mcapi_pktchan_recv_i(r1,(void **)&pbuffer,&request1,&status);
-	if (status != exp_status) { WRONG}
-	if (status == MCAPI_SUCCESS) {
+	mcapi_pktchan_recv_i(r1,(void **)&pbuffer,&request1,status);
+	if (*status == MCAPI_SUCCESS) {
 		coreb_msg("endpoint=%i has received!! \n",(int)recv);
 
-		*pbuffer=*pbuffer + 1;
+		ph = pbuffer;
+		if (ph->magic != 0xAA55)
+			coreb_msg("buffer magic is wrong\n");
+		vaddr = ph->vaddr;
+		paddr = ph->paddr;
+		size = ph->size;
+		op = ph->op;
+
+		p = (void *)paddr;
+
+		coreb_msg("##%08x %08x %08x %08x\n", vaddr, paddr, size, op);
+		if (op == 1) {
+			coreb_msg("processing buffer\n");
+			for(i = 0; i < size; i++)
+				p[i] = p[i] + 1;
+		}
+
 		if (pbuffer)
 			mcapi_pktchan_release(pbuffer, &status1);
 		mcapi_pktchan_recv_close_i(r1,&request1, &status1);
@@ -77,7 +104,7 @@ void icc_task_init(int argc, char *argv[])
   	mcapi_status_t status,status1,status2;
   	mcapi_info_t version;
   	mcapi_param_t parms;
-	mcapi_endpoint_t ep1,ep2,ep3;
+	mcapi_endpoint_t ep1,ep2,ep3,ep4;
 	int i=0;
 	int pass_num1=0,pass_num2=0;
 	mcapi_uint_t avail;
@@ -101,16 +128,20 @@ void icc_task_init(int argc, char *argv[])
 	ep3 = mcapi_endpoint_get (DOMAIN,MASTER_NODE_NUM,MASTER_PORT_NUM1,MCA_INFINITE,&status);
 	if (status != MCAPI_SUCCESS) { WRONG }
 
+	ep4 = mcapi_endpoint_get (DOMAIN,MASTER_NODE_NUM,MASTER_PORT_NUM2,MCA_INFINITE,&status);
+	if (status != MCAPI_SUCCESS) { WRONG }
+
+
 	coreb_msg("mcapi pktchan test ep3 %x\n", ep3);
 
 	i = 0;
 	while (1) {
 		if (icc_wait()) {
-			recv_pktchan(ep1,status1,MCAPI_SUCCESS);
+			recv_pktchan(ep1,&status1);
                         if (status1 == MCAPI_SUCCESS)
         	                 pass_num1++;
 
-			recv_pktchan(ep2,status2,MCAPI_SUCCESS);
+			recv_pktchan(ep2,&status2);
                         if (status2 == MCAPI_SUCCESS)
         	                 pass_num2++;
 
@@ -120,10 +151,12 @@ void icc_task_init(int argc, char *argv[])
 
 			coreb_msg("\nCoreB: mcapi pktchan test. The %i time send back,status1 %d, status2 %d . \n", i, status1,status2);
 
-			if (i == 0)
+			if (i == 0) {
 				mcapi_pktchan_connect_i(ep1,ep3,&request,&status);
 				if (status != MCAPI_SUCCESS) { WRONG }
-
+				mcapi_pktchan_connect_i(ep2,ep4,&request,&status);
+				if (status != MCAPI_SUCCESS) { WRONG }
+			}
 
 	                i++;
       			if ( i == NUM_SIZES*2 - 1 ) {
