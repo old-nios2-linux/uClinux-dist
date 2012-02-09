ef116e1a7e3642a926185881ee76543d3361f0f8
Mingquan Pan <grace.pan@analog.com>
2012-02-09 09:58:54 +0000

    [#6684] Update script: change to show git info instead of svn.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10636 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/test_scripts/startup_test.sh
+++ testsuites/test_scripts/startup_test.sh
@@ -5,9 +5,10 @@ CHECKOUT=$USER/checkouts
 
 PROTOCOL=svn
 SVN_SERVER=10.99.29.20
+GIT_PREFIX=origin
 
 DO_SWITCH=1
-INDEX=2
+INDEX=1
 
 if [ "$INDEX" == "1" ] ; then
 
@@ -16,13 +17,13 @@ if [ "$INDEX" == "1" ] ; then
 
 elif [ "$INDEX" == "2" ] ; then
 
-    UCLINUX_DIST_INDEX=branches/2009R1
-    LINUX_KERNEL_INDEX=branches/2009R1
+    UCLINUX_DIST_INDEX=branches/2011R1
+    LINUX_KERNEL_INDEX=2011R1
 
 elif [ "$INDEX" == "3" ] ; then
 
-    UCLINUX_DIST_INDEX=tags/2009R1.1-RC1
-    LINUX_KERNEL_INDEX=tags/2009R1.1-RC1
+    UCLINUX_DIST_INDEX=tags/2011R1-RC1
+    LINUX_KERNEL_INDEX=2011R1-RC1
 
 fi
 
@@ -40,7 +41,7 @@ do
 
         echo -n "Checking out kernel     " ; date
         svn checkout --ignore-externals --username anonymous $PROTOCOL://$SVN_SERVER/uclinux-dist/$UCLINUX_DIST_INDEX uclinux-dist 1>/dev/null 2>&1
-        svn checkout --username anonymous $PROTOCOL://$SVN_SERVER/linux-kernel/$LINUX_KERNEL_INDEX uclinux-dist/linux-2.6.x 1>/dev/null 2>&1
+      #  svn checkout --username anonymous $PROTOCOL://$SVN_SERVER/linux-kernel/$LINUX_KERNEL_INDEX uclinux-dist/linux-2.6.x 1>/dev/null 2>&1
 
         cd uclinux-dist
 
@@ -61,12 +62,32 @@ do
             fi
         done
 
+        git clone git://$SVN_SERVER/linux-kernel linux-2.6.x
+
+          cd linux-2.6.x
+
+            if [ "$INDEX" == "1" ] ; then
+            
+                git checkout $LINUX_KERNEL_INDEX
+            
+            elif [ "$INDEX" == "2" ] ; then
+            
+                git branch -a
+                git checkout -b $LINUX_KERNEL_INDEX $GIT_PREFIX/$LINUX_KERNEL_INDEX 
+            
+            elif [ "$INDEX" == "3" ] ; then
+            
+                git tag -l 
+                git checkout -b $LINUX_KERNEL_INDEX $LINUX_KERNEL_INDEX
+            
+            fi
+
         DO_SWITCH=0
     
     fi
+
  
-    cd $USER/test_scripts/uclinux-dist/
-    cp $CHECKOUT/uclinux-dist/testsuites/test_scripts/run_kernel_test .  
+    cd $USER/test_scripts/uclinux-dist/ 
     RUN="./run_kernel_test"
     (echo $RUN ; ) | sh
 
