ff22252ff2bd94e125a19b6061bc835a4fadf95f
Bob Liu <lliubbo@gmail.com>
2012-03-08 03:41:04 +0000

    [#6993][#6960]user: iproute2: fix compile error
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10646 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/iproute2/iproute2-2.6.35/include/linux/types.h
+++ user/iproute2/iproute2-2.6.35/include/linux/types.h
@@ -34,5 +34,7 @@ typedef __u64 __bitwise __be64;
 typedef __u16 __bitwise __sum16;
 typedef __u32 __bitwise __wsum;
 
+#define __aligned_u64 __u64 __attribute__((aligned(8)))
+
 #endif /*  __ASSEMBLY__ */
 #endif /* _LINUX_TYPES_H */
