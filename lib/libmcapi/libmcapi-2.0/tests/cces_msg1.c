3a422a91fb572b88a03da05aa23430158d45d172
steven miao <realmz6@gmail.com>
2012-06-20 10:11:36 +0000

    libmcapi: add cces msg1 test
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10720 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ lib/libmcapi/libmcapi-2.0/tests/cces_msg1.c
@@ -0,0 +1,96 @@
+/* Test: msg1
+   Description: Tests simple blocking msgsend and msgrecv calls to several endpoints
+   on a single node 
+*/
+
+#include <mcapi.h>
+#include <mca.h>
+#include <mcapi_test.h>
+#include <stdio.h>
+#include <stdlib.h> /* for malloc */
+#include <string.h>
+#include <mcapi_impl_spec.h>
+
+
+#define NUM_SIZES 4
+#define BUFF_SIZE 64
+#define DOMAIN 0
+#define NODE 0
+#define CCES_SLAVE_NODE_NUM 0
+
+#define WRONG wrong(__LINE__);
+void wrong(unsigned line)
+{
+  fprintf(stderr,"WRONG: line=%u\n", line);
+  fflush(stdout);
+  _exit(1);
+}
+char send_buf[32] = "";
+char send_string[] = "HELLO_MCAPI";
+
+void send (mcapi_endpoint_t send, mcapi_endpoint_t recv,char* msg,mcapi_status_t status,int exp_status) {
+  int size = strlen(msg);
+  int priority = 1;
+  mcapi_request_t request;
+
+  mcapi_msg_send_i(send,recv,msg,size,priority,&request,&status);
+  if (status != exp_status) { WRONG}
+  if (status == MCAPI_SUCCESS) {
+    printf("endpoint=%i has sent: [%s]\n",(int)send,msg);
+  }
+}
+
+void recv (mcapi_endpoint_t recv,mcapi_status_t status,int exp_status) {
+  size_t recv_size;
+  char buffer[BUFF_SIZE];
+  mcapi_request_t request;
+  mcapi_msg_recv_i(recv,buffer,BUFF_SIZE,&request,&status);
+  if (status != exp_status) { WRONG}
+  if (status == MCAPI_SUCCESS) {
+    printf("endpoint=%i has received: [%s]\n",(int)recv,buffer);
+  }
+}
+
+int main () {
+  mcapi_status_t status;
+  mcapi_param_t parms;
+  mcapi_info_t version;
+  mcapi_endpoint_t ep1,ep2;
+  int i,s = 0, rc = 0,pass_num=0;
+  mcapi_uint_t avail;
+
+  /* create a node */
+  mcapi_initialize(DOMAIN,NODE,NULL,&parms,&version,&status);
+  if (status != MCAPI_SUCCESS) { WRONG }
+    
+  /* create endpoints */
+  ep1 = mcapi_endpoint_create(MASTER_PORT_NUM1,&status);
+  if (status != MCAPI_SUCCESS) { WRONG }
+  printf("ep1 %x   \n", ep1);
+
+  ep2 = mcapi_endpoint_get (DOMAIN,CCES_SLAVE_NODE_NUM,0,MCA_INFINITE,&status);
+  if (status != MCAPI_SUCCESS) { WRONG }
+  printf("ep2 %x   \n", ep2);
+
+  /* send and recv messages on the endpoints */
+  /* regular endpoints */
+
+  for (s = 0; s < NUM_SIZES; s++) {
+  sprintf(send_buf, "%s %d", send_string, s);
+  send (ep1,ep2,send_buf,status,MCAPI_SUCCESS);
+  if (status != MCAPI_SUCCESS) { WRONG }
+  printf("coreA: The %d time sending, status %d\n",s, status);
+
+  }
+  
+  mcapi_endpoint_delete(ep1,&status);
+
+  mcapi_finalize(&status);
+
+  if (pass_num == NUM_SIZES )
+  printf("CoreA Test PASSED\n");
+  else
+  printf("CoreA Test FAILED\n");
+
+  return 0;
+}
