00fb423afe44baa82d9d2a9a10e03cca9b358c09
steven miao <realmz6@gmail.com>
2012-06-20 06:56:56 +0000

    icc core: debug, check fault address
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10719 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/icc_utils/icc_core/corebboot.c
+++ user/blkfin-apps/icc_utils/icc_core/corebboot.c
@@ -314,6 +314,10 @@ void dump_execption(unsigned int errno, unsigned int addr)
 	coreb_msg("execption %x addr %x\n", errno, addr);
 	if (errno == 0x26) {
 		unsigned long fault_addr = bfin_read_DCPLB_FAULT_ADDR();
+		if ((fault_addr >= 0x8000000) || (fault_addr < L2_START)
+				|| (fault_addr >= L2_START + L2_LENGTH)) {
+			dump_stack();
+		}
 		_disable_dcplb();
 		fault_addr &= ~(0x400000 - 1);
 		bfin_write32(DCPLB_ADDR1 + 4 * ((fault_addr/ 0x1000000) % 15), fault_addr);
@@ -322,6 +326,10 @@ void dump_execption(unsigned int errno, unsigned int addr)
 		coreb_msg("fault addr %x dcplb %d\n", fault_addr, (fault_addr/ 0x1000000) % 15);
 	} else if (errno == 0x2c) {
 		unsigned long fault_addr = bfin_read_ICPLB_FAULT_ADDR();
+		if ((fault_addr >= 0x8000000) || (fault_addr < L2_START)
+				|| (fault_addr >= L2_START + L2_LENGTH)) {
+			dump_stack();
+		}
 		_disable_icplb();
 		fault_addr &= ~(0x400000 - 1);
 		bfin_write32(ICPLB_ADDR1 + 4 * ((fault_addr/ 0x1000000) % 15), fault_addr);
