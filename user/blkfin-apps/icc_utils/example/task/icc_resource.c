dd961bb109699465abf79725af4b188124f36cf3
steven miao <realmz6@gmail.com>
2012-12-18 09:27:02 +0000

    icc resource test: add peripheral resource data
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10832 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/icc_utils/example/task/icc_resource.c
+++ user/blkfin-apps/icc_utils/example/task/icc_resource.c
@@ -1,27 +1,50 @@
 #include <icc.h>
 #include <protocol.h>
 #include <debug.h>
+#include <generated/autoconf.h>
+#include <asm/portmux.h>
 
 #define TEST_NUM 4
 
-uint32_t __icc_task_data resource_array[] = {
-	RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 2),
+unsigned short bfin_peripheral_list[] = {P_SPI1_SCK, P_SPI1_MISO, P_SPI1_MOSI, 0};
+resources_t bfin_peri_res = {
+	.label = "bfin-spi1",
+};
+
+uint32_t resourceid_array[] = {
 	RESMGR_ID(RESMGR_TYPE_GPIO, 40),
 	RESMGR_ID(RESMGR_TYPE_SYS_IRQ, 52),
 	RESMGR_ID(RESMGR_TYPE_DMA, 20),
 	0,
 };
 
-void  __icc_task icc_task_init(int argc, char *argv[])
+void icc_task_init(int argc, char *argv[])
 {
 	int ret;
 	int i = 0;
 
+	bfin_peri_res.count = 3;
+	bfin_peri_res.resources_array = (uint32_t)bfin_peripheral_list;
+
+	COREB_DEBUG(1, "request resource id %s\n", bfin_peri_res.label);
+	ret = sm_request_resource(EP_RESMGR_SERVICE, RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 0), &bfin_peri_res);
+	if (ret) {
+		COREB_DEBUG(1, "request peri resource failed\n");
+	}
+
+	ret = sm_free_resource(EP_RESMGR_SERVICE, RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 0), &bfin_peri_res);
+	if (ret) {
+		COREB_DEBUG(1, "free peri resource failed\n");
+	}
+
 	while (i < TEST_NUM) {
 
-		COREB_DEBUG(1, "request resource id %08x\n", resource_array[i]);
-		ret = sm_request_resource(0, resource_array[i]);
-		if (ret <= 0) {
+		if (resourceid_array[i] == 0)
+			break;
+
+		COREB_DEBUG(1, "request resource id %08x\n", resourceid_array[i]);
+		ret = sm_request_resource(EP_RESMGR_SERVICE, resourceid_array[i], 0);
+		if (ret) {
 			COREB_DEBUG(1, "request resource failed\n");
 		}
 		i++;
@@ -31,10 +54,13 @@ void  __icc_task icc_task_init(int argc, char *argv[])
 	i = 0;
 	while (i < TEST_NUM) {
 
-		COREB_DEBUG(1, "request resource id %08x\n", resource_array[i]);
-		ret = sm_free_resource(0, resource_array[i]);
-		if (ret <= 0) {
-			COREB_DEBUG(1, "request resource failed\n");
+		if (resourceid_array[i] == 0)
+			break;
+
+		COREB_DEBUG(1, "request resource id %08x\n", resourceid_array[i]);
+		ret = sm_free_resource(EP_RESMGR_SERVICE, resourceid_array[i], 0);
+		if (ret) {
+			COREB_DEBUG(1, "free resource failed\n");
 		}
 		i++;
 	}
@@ -43,7 +69,7 @@ void  __icc_task icc_task_init(int argc, char *argv[])
 	COREB_DEBUG(1, "%s() end\n", __func__);
 }
 
-void  __icc_task icc_task_exit(void)
+void icc_task_exit(void)
 {
 }
 
