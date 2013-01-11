7bd22d7256adeb8aa4b5a29673c913335ca2f0c7
steven miao <realmz6@gmail.com>
2013-01-11 02:17:37 +0000

    icc: add irq table and descriptor, refine icc resource task
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@10834 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/icc_utils/example/task/icc_resource.c
+++ user/blkfin-apps/icc_utils/example/task/icc_resource.c
@@ -5,6 +5,21 @@
 #include <asm/portmux.h>
 
 #define TEST_NUM 4
+enum {
+	IRQ_TYPE_NONE           = 0x00000000,
+	IRQ_TYPE_EDGE_RISING    = 0x00000001,
+	IRQ_TYPE_EDGE_FALLING   = 0x00000002,
+	IRQ_TYPE_EDGE_BOTH      = (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_EDGE_RISING),
+	IRQ_TYPE_LEVEL_HIGH     = 0x00000004,
+	IRQ_TYPE_LEVEL_LOW      = 0x00000008,
+	IRQ_TYPE_LEVEL_MASK     = (IRQ_TYPE_LEVEL_LOW | IRQ_TYPE_LEVEL_HIGH),
+	IRQ_TYPE_SENSE_MASK     = 0x0000000f,
+	IRQ_TYPE_DEFAULT        = IRQ_TYPE_SENSE_MASK,
+
+	IRQ_TYPE_PROBE          = 0x00000010,
+
+	IRQ_LEVEL               = (1 <<  8),
+};
 
 unsigned short bfin_peripheral_list[] = {P_SPI1_SCK, P_SPI1_MISO, P_SPI1_MOSI, 0};
 resources_t bfin_peri_res = {
@@ -12,11 +27,84 @@ resources_t bfin_peri_res = {
 };
 
 uint32_t resourceid_array[] = {
-	RESMGR_ID(RESMGR_TYPE_GPIO, 40),
-	RESMGR_ID(RESMGR_TYPE_SYS_IRQ, 52),
-	RESMGR_ID(RESMGR_TYPE_DMA, 20),
+	RESMGR_ID(RESMGR_TYPE_SYS_IRQ, IRQ_PINT4),
 	0,
 };
+
+void handle_gpio_irq(uint32_t pin)
+{
+	COREB_DEBUG(1, "handle gpio irq %d\n", pin);
+}
+
+static int bfin_handle_pint_irq(int irq)
+{
+	unsigned int bank;
+	volatile struct bfin_pint_regs *pint;
+	uint32_t pint_assign, pint_request, request, level_mask;
+	uint32_t bit = 0;
+	uint32_t pin = 0;
+
+	bank = irq - IRQ_PINT0;
+	pint = (struct bfin_pint_regs *)(PINT0_MASK_SET + 0x100 * bank);
+	pint_assign = pint->assign;
+	pint_request = request = pint->request;
+	level_mask = pint->edge_set & request;
+
+	pint->request = pint_request;
+	pint->mask_clear = pint_request;
+
+	while (request) {
+		if (request & 1) {
+			pin = 16 * bank + 16 * ((pint_assign >> (bit/8 * 8)) & 0xff) + (bit % 16);
+			COREB_DEBUG(1, "pint irq %d %d\n", bank, bit);
+			handle_gpio_irq(pin);
+		}
+		bit++;
+		request >>= 1;
+	}
+
+	pint->mask_set = pint_request;
+}
+
+static int bfin_setup_pint_irq(int irq, int pin, unsigned long type)
+{
+	unsigned int bank;
+	volatile struct bfin_pint_regs *pint;
+	volatile struct gpio_port_t * gpiobank_reg;
+	uint32_t pintbit;
+	uint32_t gpiobit;
+
+	bank = irq - IRQ_PINT0;
+
+	pintbit = 1 << ((pin + 16 * (pint->assign & 0xf)) & 0x1F);
+	gpiobit = 1 << (pin & 0x1F);
+
+	pint = (struct bfin_pint_regs *)(PINT0_MASK_SET + 0x100 * bank);
+
+	gpiobank_reg = (struct gpio_port_t *)(PORTA_FER + 0x80 * bank);
+
+	gpiobank_reg->dir_clear = gpiobit;
+	gpiobank_reg->inen |= gpiobit;
+
+
+	if (type & (IRQ_TYPE_EDGE_RISING | IRQ_TYPE_EDGE_FALLING)) {
+		pint->edge_set = pintbit;
+	} else {
+		pint->edge_clear = pintbit;
+	}
+
+	if ((type & (IRQ_TYPE_EDGE_FALLING | IRQ_TYPE_LEVEL_LOW))) {
+		pint->invert_set = pintbit;
+	} else {
+		pint->invert_clear = pintbit;
+	}
+
+	pint->mask_set = pintbit;
+
+	SSYNC();
+
+	return 0;
+}
 
 void icc_task_init(int argc, char *argv[])
 {
@@ -27,49 +115,43 @@ void icc_task_init(int argc, char *argv[])
 	bfin_peri_res.resources_array = (uint32_t)bfin_peripheral_list;
 
 	COREB_DEBUG(1, "request resource id %s\n", bfin_peri_res.label);
-	ret = sm_request_resource(EP_RESMGR_SERVICE, RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 0), &bfin_peri_res);
+	ret = sm_request_resource(0, RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 0), &bfin_peri_res);
 	if (ret) {
 		COREB_DEBUG(1, "request peri resource failed\n");
 	}
 
-	ret = sm_free_resource(EP_RESMGR_SERVICE, RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 0), &bfin_peri_res);
-	if (ret) {
-		COREB_DEBUG(1, "free peri resource failed\n");
-	}
-
 	while (i < TEST_NUM) {
 
 		if (resourceid_array[i] == 0)
 			break;
 
 		COREB_DEBUG(1, "request resource id %08x\n", resourceid_array[i]);
-		ret = sm_request_resource(EP_RESMGR_SERVICE, resourceid_array[i], 0);
+		ret = sm_request_resource(0, resourceid_array[i], 0);
 		if (ret) {
 			COREB_DEBUG(1, "request resource failed\n");
 		}
+
 		i++;
 
 	}
 
-	i = 0;
-	while (i < TEST_NUM) {
-
-		if (resourceid_array[i] == 0)
-			break;
-
-		COREB_DEBUG(1, "request resource id %08x\n", resourceid_array[i]);
-		ret = sm_free_resource(EP_RESMGR_SERVICE, resourceid_array[i], 0);
-		if (ret) {
-			COREB_DEBUG(1, "free resource failed\n");
-		}
-		i++;
-	}
-
+	icc_register_irq_handle(IRQ_PINT4, bfin_handle_pint_irq);
+	bfin_setup_pint_irq(IRQ_PINT4, 12, IRQ_TYPE_EDGE_RISING);
 
 	COREB_DEBUG(1, "%s() end\n", __func__);
 }
 
 void icc_task_exit(void)
 {
+	int i;
+	sm_free_resource(0, RESMGR_ID(RESMGR_TYPE_PERIPHERAL, 0), &bfin_peri_res);
+
+	while (i < TEST_NUM) {
+		if (resourceid_array[i] == 0)
+			break;
+		sm_free_resource(0, resourceid_array[i], 0);
+	}
+
+	icc_unregister_irq_handle(IRQ_PINT4);
 }
 
--- user/blkfin-apps/icc_utils/include/protocol.h
+++ user/blkfin-apps/icc_utils/include/protocol.h
@@ -5,21 +5,32 @@
 
 #define MSGQ_START_ADDR L2_START
 
+struct icc_irq_desc {
+	int (*handle)(int irq, void *data);
+	void *data;
+	unsigned int flags;
+};
+
 struct coreb_icc_node {
 	struct sm_icc_desc icc_info;
 	struct sm_session_table *sessions_table;
 	struct gen_pool *pool;
 	struct gen_pool *msg_pool;
+	struct icc_irq_desc *irq_table;
 };
+
 
 extern struct coreb_icc_node coreb_info;
 
-int init_sm_session_table(void);
-
+void init_sm_session_table(void);
+void init_icc_irq_table(void);
 
 int sm_send_packet(uint32_t session_idx, uint16_t dst_ep,
 		uint16_t dst_cpu, void *buf, uint32_t len);
 
 int icc_get_session_status(void *user_param, uint32_t session_idx);
+
+int icc_register_irq_handle(int irq, int (*handle)(int irq, void *data));
+void icc_unregister_irq_handle(int irq);
 
 #endif
